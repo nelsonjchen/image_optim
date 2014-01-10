ca49be9a48a130c6e7f87e0f9e2eab739c2a4ec5
Nelson Chen <crazysim@gmail.com>
2014-01-09 23:33:29 -0800

    Simplify zopflipng parameters.

--- lib/image_optim/worker/zopfli.rb
+++ lib/image_optim/worker/zopfli.rb
@@ -7,15 +7,9 @@ class ImageOptim
         [:png]
       end
 
-      option(:splitlast, false, 'Do block splitting last instead of first'){ |v| !!v }
-
-      option(:iterations, 15, 'Perform # iterations (default 15). More gives more compression but is slower. Examples: 10, 50, 1000') do |v|
-        OptionHelpers.limit_with_range(v.to_i, 1..1000)
-      end
-
       def optimize(src, dst)
-        args = %W[-c #{"--splitlast" if splitlast} -i#{iterations} #{src} > #{dst}]
-        execute(:zopfli, *args) && optimized?(src, dst)
+        args = %W[-m #{src} #{dst}]
+        execute(:zopflipng, *args) && optimized?(src, dst)
       end
     end
   end
